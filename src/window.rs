be596974c14cd479bf0e196c11ade0909ca32a81
Phill <philltv.gaming@gmail.com>
2023-11-05 21:37:25 +0100

    feat: add window struct and simple window layout

--- src/window.rs
+++ src/window.rs
@@ -1,29 +1,123 @@
+use crate::wad::{FileRecord, WadRework};
 use eframe::{
-    egui::{CentralPanel, SidePanel},
+    egui::{CentralPanel, Layout, RichText, ScrollArea, SidePanel, TextEdit, TopBottomPanel},
+    emath::Align,
     App,
 };
 
 #[derive(Default)]
-pub struct Window {}
+pub struct Window {
+    file_search: String,
+    files: Vec<FileRecord>,
+    wad_path: String,
+    selected_record: String,
+    selected_record_content: String,
+    invalid_file_found: bool,
+}
 
 impl Window {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+        Self {
+            ..Default::default()
+        }
     }
 }
 
 impl App for Window {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
-        draw_left_panel(&ctx);
-        draw_center_panel(&ctx);
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.with_layout(Layout::left_to_right(Align::LEFT), |ui| {
+                ui.label("Path:");
+                ui.add(TextEdit::singleline(&mut self.wad_path).hint_text("Path of the .wad file"));
+                if ui.button("Open file").clicked() {
+                    if !self.wad_path.is_empty() {
+                        match std::fs::read(&self.wad_path) {
+                            Ok(mut buffer) => match WadRework::new(&mut buffer) {
+                                Ok(wad) => {
+                                    self.files = wad.files.values().cloned().collect();
+                                    for ele in &self.files {
+                                        println!("{}", ele.file_name);
+                                    }
+                                    self.invalid_file_found = false;
+                                }
+                                Err(_) => self.invalid_file_found = true,
+                            },
+                            Err(_) => self.invalid_file_found = true,
+                        }
+                    }
+                }
+
+                if self.invalid_file_found {
+                    ui.label(
+                        RichText::new("Invalid file provided!").color(ui.visuals().error_fg_color),
+                    );
+                }
+
+                ui.with_layout(Layout::right_to_left(Align::RIGHT), |ui| {
+                    ui.label(format!("{} Files", self.files.len()));
+                })
+            });
+        });
+
+        SidePanel::left("left_panel").show(ctx, |ui| {
+            ui.set_min_width(200.0);
+
+            ScrollArea::vertical().show(ui, |ui| {
+                if !self.files.is_empty() {
+                    ui.with_layout(Layout::left_to_right(Align::LEFT), |ui| {
+                        ui.label("Search");
+                        ui.add(
+                            TextEdit::singleline(&mut self.file_search).hint_text("Something.txt"),
+                        );
+                    });
+
+                    // CollapsingHeader::new("WAD-File")
+                    // .default_open(self.files.len() > 0)
+                    // .show(ui, |ui| {
+                    //     for el in &self.files.iter().cloned().collect::<Vec<FileRecord>>() {
+                    //         // recursive_show(ui, el, &el.file_name, self);
+                    //     }
+                    // });
+                }
+            });
+        });
+
+        CentralPanel::default().show(ctx, |ui| {
+            if self.selected_record_content.len() > 0 {
+                ui.text_edit_multiline(&mut format!("{:#?}", self.selected_record_content));
+            } else {
+                self.selected_record_content = self.selected_record.clone();
+            }
+        });
     }
 }
 
-fn draw_left_panel(ctx: &eframe::egui::Context) {
-    SidePanel::left("left_panel").show(ctx, |ui| {
-        //
-    });
-}
-fn draw_center_panel(ctx: &eframe::egui::Context) {
-    CentralPanel::default().show(ctx, |ui| {});
-}
+// fn recursive_show(ui: &mut Ui, input: &FileRecord, full_file_name: &String, wnd: &mut Window) {
+//     if input.file_name.contains("/") {
+//         let path = &input.file_name.split("/").collect::<Vec<&str>>();
+//         let file_name = path.first().unwrap().to_string();
+//         let new_path = &input.file_name[file_name.len() + 1..];
+
+//         CollapsingHeader::new(file_name.clone())
+//             .id_source(Id::with(ui.id(), full_file_name))
+//             .default_open(false)
+//             .show(ui, |ui| {
+//                 let record = FileRecord {
+//                     file_name: new_path.to_string(),
+//                     ..*input
+//                 };
+
+//                 recursive_show(ui, &record, full_file_name, wnd);
+//             });
+//     } else {
+//         if ui.selectable_label(false, &input.file_name).clicked() {
+//             wnd.selected_record = full_file_name.to_owned();
+//             wnd.selected_record_content = String::from("");
+//         }
+//     }
+// }
+
+// Input:
+// QuestData/KR/KR-LAST-C18-001.xml
+// QuestData/WC/WC-ICE-C04-001.xml
+// QuestArcData/KT-CRY5-C01.xml
