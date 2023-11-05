123f4e4420a466197e9ece57dac96c7d51704f63
Phill <philltv.gaming@gmail.com>
2023-11-05 21:37:35 +0100

    feat: add dark window style

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use eframe::{run_native, NativeOptions};
+use eframe::{
+    egui::{Style, Visuals},
+    run_native, NativeOptions,
+};
 use window::Window;
 
+pub mod wad;
 mod window;
+
 fn main() {
     let win_option = NativeOptions::default();
     run_native(
         "VoiceClient",
         win_option,
-        Box::new(move |cc| Box::new(Window::new(cc))),
+        Box::new(move |cc| {
+            let style = Style {
+                visuals: Visuals::dark(),
+                ..Style::default()
+            };
+            cc.egui_ctx.set_style(style);
+            Box::new(Window::new(cc))
+        }),
     )
     .unwrap();
 }
